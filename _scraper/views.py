74b7afcd7e63a3094aa214ba6c3938a90aedd219
Dan Davison <dan@counsyl.com>
2012-11-21 00:12:07 -0800

    Add node executable from ubuntu
    Linux testp-dan 2.6.32-40-generic #87-Ubuntu SMP Tue Mar 6 00:56:56 UTC 2012 x86_64 GNU/Linux
    /usr/local/node/bin/node: ELF 64-bit LSB executable, x86-64, version 1 (SYSV), dynamically linked (uses shared libs), for GNU/Linux 2.6.15, not stripped

--- _scraper/views.py
+++ _scraper/views.py
@@ -83,7 +83,10 @@ def get_scrape_data():
     scrape = os.path.join(settings.SITE_DIRECTORY,
                           'js/scrape.js')
 
-    scraper = Popen(['node', scrape], stdin=PIPE, stdout=PIPE)
+    node = os.path.join(settings.SITE_DIRECTORY,
+                        'bin/node')
+
+    scraper = Popen([node, scrape], stdin=PIPE, stdout=PIPE)
     scraper.stdin.close()
 
     data = scraper.stdout.read()
