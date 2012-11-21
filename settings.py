51298914091bac7d18024904c8281b4e746e5b68
Dan Davison <dan@counsyl.com>
2012-11-20 23:44:28 -0800

    Change root project name in settings.py

--- settings.py
+++ settings.py
@@ -107,7 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'scraper.urls'
+ROOT_URLCONF = 'scrapey.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
