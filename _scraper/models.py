7ef75fe8186a13e5ef5bc64f3dc20463ca8e96c1
Dan Davison <dan@counsyl.com>
2012-11-21 22:40:26 -0800

    Revert "Temporarily delete model to force heroku to drop the table"
    This reverts commit f44c075b16e3db1680f1b88d815aad5bb36d3c20.

--- _scraper/models.py
+++ _scraper/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 
+class Article(models.Model):
+    site = models.CharField(max_length=50)
+    title = models.CharField(max_length=500)
+    pub_date = models.DateTimeField(null=True)
