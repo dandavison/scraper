f44c075b16e3db1680f1b88d815aad5bb36d3c20
Dan Davison <dan@counsyl.com>
2012-11-21 22:39:19 -0800

    Temporarily delete model to force heroku to drop the table

--- _scraper/models.py
+++ _scraper/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 
-class Article(models.Model):
-    site = models.CharField(max_length=50)
-    title = models.CharField(max_length=500)
-    pub_date = models.DateTimeField(null=True)
