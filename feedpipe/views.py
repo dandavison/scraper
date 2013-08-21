8c3d71722be5b2a2336c92d6f489e4b59bc401df
Dan Davison <dan@counsyl.com>
2013-08-20 22:48:52 -0700

    Modify politix homepage feed

--- feedpipe/views.py
+++ feedpipe/views.py
@@ -1,3 +1,5 @@
+from lxml import etree
+from StringIO import StringIO
 import requests
 
 from django.http import HttpResponse
@@ -7,5 +9,29 @@ def politix_homepage(request):
     url = 'http://politix.topix.com/rssfeeds/homepage'
     resp = requests.get(url)
     resp.raise_for_status()
-    feed = resp.content
-    return HttpResponse(content=feed, content_type='text/xml', status=200)
+    rss = etree.fromstring(resp.content)
+    _process_politix_homepage_feed(rss)
+    buf = StringIO()
+    rss.getroottree().write(buf)
+    return HttpResponse(
+        content=buf.getvalue(),
+        content_type='text/xml',
+        status=200,
+    )
+
+
+def _process_politix_homepage_feed(rss):
+    for el in rss.getiterator():
+        if el.tag == 'item':
+            _process_politix_homepage_item(el)
+
+
+def _process_politix_homepage_item(item):
+    creator = item.find('{http://purl.org/dc/elements/1.1/}creator')
+    if creator is None:
+        return
+    title = item.find('title')
+    if title is None:
+        print "Expected title element"
+    else:
+        title.text += ' @Politix%s' % creator.text.split()[0]
